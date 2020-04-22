6ce03a1f1930b85bda023a3ca5e2e776386a1d61
pr4shantS0naw4n3 <prashant.sonawane@18northsystems.com>
2020-04-22 19:05:37 +0530

    Home page login check

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Component, OnInit, Renderer2 } from '@angular/core';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
@@ -17,26 +19,30 @@ export class ComponentsComponent implements OnInit {
     focus;
     focus1;
     focus2;
-    date: {year: number, month: number};
+    date: { year: number, month: number };
     model: NgbDateStruct;
-    constructor( private renderer : Renderer2) {}
+    constructor(private cookieService: CookieService, private router: Router, private renderer: Renderer2) { }
     isWeekend(date: NgbDateStruct) {
         const d = new Date(date.year, date.month - 1, date.day);
         return d.getDay() === 0 || d.getDay() === 6;
     }
 
-    isDisabled(date: NgbDateStruct, current: {month: number}) {
+    isDisabled(date: NgbDateStruct, current: { month: number }) {
         return date.month !== current.month;
     }
 
     ngOnInit() {
+        const vToken = this.cookieService.get('token');
+        if (vToken) {
+            this.router.navigate(['/user-profile']);
+        }
         let input_group_focus = document.getElementsByClassName('form-control');
         let input_group = document.getElementsByClassName('input-group');
         for (let i = 0; i < input_group.length; i++) {
-            input_group[i].children[0].addEventListener('focus', function (){
+            input_group[i].children[0].addEventListener('focus', function () {
                 input_group[i].classList.add('input-group-focus');
             });
-            input_group[i].children[0].addEventListener('blur', function (){
+            input_group[i].children[0].addEventListener('blur', function () {
                 input_group[i].classList.remove('input-group-focus');
             });
         }
